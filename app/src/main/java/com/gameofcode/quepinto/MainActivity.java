9c502374312bd1765f7c120918b3fea0af5c7861
aldojdg@gmail.com <Aeternio117>
2021-03-31 02:22:52 -0300

    logo app conexión entre actividades

--- app/src/main/java/com/gameofcode/quepinto/MainActivity.java
+++ app/src/main/java/com/gameofcode/quepinto/MainActivity.java
@@ -3,8 +3,11 @@ package com.gameofcode.quepinto;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
-
+import android.content.Intent;
 import com.gameofcode.quepinto.DTO.UsuarioDTO;
 import com.gameofcode.quepinto.helpers.ConnectDBHelper;
 import com.gameofcode.quepinto.models.UsuarioModel;
@@ -13,18 +16,28 @@ import java.sql.SQLException;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    Button BtnEven;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Toast.makeText(this,"esto es solo un prueba, los stings van en el XML",Toast.LENGTH_LONG).show();
         doStaff();
-
+        EditText UsuNom = findViewById(R.id.TxtUsuNom);
+        EditText UsuPwd = findViewById(R.id.TxtUsuPas);
+        BtnEven = findViewById(R.id.BtnEven);
 
 
 
     }
+    public void buscarEventos(View view) {
+        Intent buscar = new Intent(this,MainActivity2.class);
+        startActivity(buscar);}
+
+    public void noRegistro(View view) {
+        Intent registro = new Intent(this,MainActivity3.class);
+        startActivity(registro);}
+
 
     private void doStaff(){
         new Thread(new Runnable() {
