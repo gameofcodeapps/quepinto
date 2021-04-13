a1f3755aadddfa9e2774838844e882c81edad92b
Stephanie Barbitta <stephaniebarbitta@gmail.com>
2021-04-12 21:45:08 -0300

    Evento Single y Listado de Eventos (still incomplete)

---
+++ app/src/main/java/com/gameofcode/quepinto/MainActivityEvento.java
@@ -0,0 +1,14 @@
+package com.gameofcode.quepinto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivityEvento extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_evento);
+    }
+}
