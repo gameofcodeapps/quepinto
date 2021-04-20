c701eca9464feee60e74636b38b62eaeeec89529
Charles <charles.zoryez@gmail.com>
2021-04-19 21:09:38 -0300

    Registro

--- app/src/main/java/com/gameofcode/quepinto/MainActivityRegEvento.java
+++ app/src/main/java/com/gameofcode/quepinto/MainActivityRegEvento.java
@@ -2,13 +2,115 @@ package com.gameofcode.quepinto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
-public class MainActivityRegEvento extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+
+public class MainActivityRegEvento extends AppCompatActivity implements OnMapReadyCallback, AdapterView.OnItemSelectedListener, View.OnClickListener {
+
+
+    private GoogleMap mMap;
+    Button bfecha,bhora;
+    EditText efecha,ehora;
+    private int dia,mes,ano,hora,minutos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_reg_evento);
+
+        bfecha=(Button)findViewById(R.id.btnFecha);
+        bhora=(Button)findViewById(R.id.btnHora);
+        efecha=(EditText) findViewById(R.id.txtFecha);
+        ehora=(EditText) findViewById(R.id.txtHora);
+        bfecha.setOnClickListener(this);
+        bhora.setOnClickListener(this);
+
+        Spinner spinner = findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.Categorias, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions()
+                .position(sydney)
+                .title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 12));
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v==bfecha){
+            final Calendar c = Calendar.getInstance();
+            dia=c.get(Calendar.DAY_OF_MONTH);
+            mes=c.get(Calendar.MONTH);
+            ano=c.get(Calendar.YEAR);
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                    efecha.setText(dayOfMonth+"/"+month+"/"+year);
+
+                }
+            }
+            ,dia,mes,ano);
+            datePickerDialog.show();
+        }
+        if (v==bhora){
+            final Calendar c = Calendar.getInstance();
+            hora=c.get(Calendar.HOUR_OF_DAY);
+            minutos=c.get(Calendar.MINUTE);
+
+            TimePickerDialog timePickerDialog = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+                @Override
+                public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                    ehora.setText(hourOfDay+":"+minute);
+                }
+            },hora,minutos,false);
+            timePickerDialog.show();
+        }
     }
 }
