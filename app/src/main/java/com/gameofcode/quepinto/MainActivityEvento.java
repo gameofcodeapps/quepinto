3af8c87c56fec8975c0d4eba0aebb8f4c1e03939
aldojdg@gmail.com <Aeternio117>
2021-04-25 00:27:17 -0300

    Compartir

--- app/src/main/java/com/gameofcode/quepinto/MainActivityEvento.java
+++ app/src/main/java/com/gameofcode/quepinto/MainActivityEvento.java
@@ -3,13 +3,18 @@ package com.gameofcode.quepinto;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivityEvento extends AppCompatActivity {
     ImageView imgEvento;
     TextView txtNomEvento,txtFecha,txtdscEvento,txtOrganizador,txtMapa;
+    ImageButton share;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +32,22 @@ public class MainActivityEvento extends AppCompatActivity {
         txtdscEvento.setText(getIntent().getStringExtra("desc"));
         txtOrganizador.setText(getIntent().getStringExtra("organizador"));
         txtMapa.setText(getIntent().getStringExtra("mapa"));
+
+        share=(ImageButton)findViewById(R.id.share);
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent shareIntent = new Intent(Intent.ACTION_SEND);
+                shareIntent.setType("text/plain");
+                String shareBody="Accede a nuestra aplicación desde aquí; http://quepinto.pythonanywhere.com";
+                String shareSub="QuePintó?";
+
+                shareIntent.putExtra(Intent.EXTRA_SUBJECT,shareSub);
+                shareIntent.putExtra(Intent.EXTRA_TEXT,shareBody);
+
+                startActivity(Intent.createChooser(shareIntent,"Compartir usando:"));
+            }
+        });
     }
 
 }
--- app/src/main/java/com/gameofcode/quepinto/MainActivityListadoEvento.java
+++ app/src/main/java/com/gameofcode/quepinto/MainActivityListadoEvento.java
@@ -3,13 +3,17 @@ package com.gameofcode.quepinto;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivityListadoEvento extends AppCompatActivity {
     ImageView imgEvento;
     TextView txtNomEvento,txtFecha;
+    ImageButton share;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +25,21 @@ public class MainActivityListadoEvento extends AppCompatActivity {
         imgEvento.setImageResource(getIntent().getIntExtra("Imagen Evento",0));
         txtNomEvento.setText(getIntent().getStringExtra("Evento"));
         txtFecha.setText(getIntent().getStringExtra("Fecha y Hora Evento"));
+        share=(ImageButton)findViewById(R.id.share);
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent shareIntent = new Intent(Intent.ACTION_SEND);
+                shareIntent.setType("text/plain");
+                String shareBody="Accede a nuestra aplicación desde aquí; http://quepinto.pythonanywhere.com";
+                String shareSub="QuePintó?";
+
+                shareIntent.putExtra(Intent.EXTRA_SUBJECT,shareSub);
+                shareIntent.putExtra(Intent.EXTRA_TEXT,shareBody);
+
+                startActivity(Intent.createChooser(shareIntent,"Compartir usando:"));
+            }
+        });
     }
 
 }
