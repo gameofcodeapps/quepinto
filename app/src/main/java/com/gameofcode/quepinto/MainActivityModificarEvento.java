e056cc226d78c75b744c9fb9b1653ee5540edbc1
Charles <charles.zoryez@gmail.com>
2021-04-26 01:50:39 -0300

    Modificar evento xml

---
+++ app/src/main/java/com/gameofcode/quepinto/MainActivityModificarEvento.java
@@ -0,0 +1,14 @@
+package com.gameofcode.quepinto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivityModificarEvento extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_modificar_evento);
+    }
+}
