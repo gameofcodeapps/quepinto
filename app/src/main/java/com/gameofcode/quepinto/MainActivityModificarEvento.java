488e193af80bb506acbc70e35a9e7d2f2b31f899
Charles <charles.zoryez@gmail.com>
2021-04-26 13:07:54 -0300

    Modificar evento xml 2

--- app/src/main/java/com/gameofcode/quepinto/MainActivityModificarEvento.java
+++ app/src/main/java/com/gameofcode/quepinto/MainActivityModificarEvento.java
@@ -7,6 +7,8 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
 
 import com.gameofcode.quepinto.DTO.EventoDTO;
 
@@ -18,7 +20,26 @@ public class MainActivityModificarEvento extends AppCompatActivity {
         setContentView(R.layout.activity_main_modificar_evento);
         EventoDTO evento = (EventoDTO) getIntent().getSerializableExtra("evento");
         EditText nombreEvento = (EditText) findViewById(R.id.nombreEvento);
+        EditText organizador = (EditText) findViewById(R.id.organizador);
+        Spinner categorias = (Spinner) findViewById(R.id.spinner);
+        EditText fecha = (EditText) findViewById(R.id.txtFecha);
+        EditText hora = (EditText) findViewById(R.id.txtHora);
+        ImageView imagen = (ImageView) findViewById(R.id.imageView);
+        EditText descripcion = (EditText) findViewById(R.id.descripcion);
+        EditText departamento = (EditText) findViewById(R.id.departamento);
+        EditText ciudad = (EditText) findViewById(R.id.ciudad);
+        EditText direccion = (EditText) findViewById(R.id.direccion);
         nombreEvento.setText(evento.getNombreEvento());
+
+        organizador.setText(evento.getOrganizador());
+        fecha.setText(evento.getFechaInicio());
+        hora.setText(evento.getHoraInicio());
+        descripcion.setText(evento.getDescripcion());
+        departamento.setText(evento.getDepartamento());
+        ciudad.setText(evento.getCiudad());
+        direccion.setText(evento.getDireccion());
+
+
 
         Button botonModificar = (Button) findViewById(R.id.btnModificarEvento);
         botonModificar.setOnClickListener(new View.OnClickListener() {
